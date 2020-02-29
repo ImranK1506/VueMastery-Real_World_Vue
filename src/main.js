6ce7ed969a919816071a74895e19a355c040e78a
Imran Khan <imran.khan@kpn.com>
2020-02-29 16:20:04 +0100

    global components

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import BaseIcon from "@/components/BaseIcon";
+
+// Global component registration
+Vue.component('BaseIcon', BaseIcon);
 
 Vue.config.productionTip = false;
 
