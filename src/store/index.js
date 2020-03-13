efca6842f1e0fb8fc652f902106b2ae670a54c6c
Imran Khan <imran.khan@kpn.com>
2020-03-13 18:30:18 +0100

    mutations and actions

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,53 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import EventService from "@/services/EventService";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  state: {
+    user: { id: '1', name: 'Imran Khan'},
+    categories: [
+      'sustainability',
+      'nature',
+      'animal welfare',
+      'housing',
+      'education',
+      'food',
+      'community'
+    ],
+    events: [
+      { id: 1, text: '...', done: true },
+      { id: 2, text: '...', done: false },
+      { id: 3, text: '...', done: true },
+      { id: 4, text: '...', done: false }
+    ]
+  },
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event.data)
+      });
+    }
+  },
+  modules: {},
+  getters: {
+    catLength: state => {
+      return state.categories.length
+    },
+    // doneTodos: state => {
+    //   return state.todos.filter(todo => todo.done)
+    // },
+    // activeTodosCount: state => {
+    //   return state.todos.filter(todo => !todo.done).length
+    // }
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  }
 });
--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,5 +1,124 @@
 <template>
-   <h1>Create Event</h1>
+   <div>
+      <h1>Create Event</h1>
+
+      <form @submit.prevent="createEvent">
+         <label>Select a category</label>
+         <select v-model="event.category">
+            <option v-for="category in categories" :key="category">{{ category }}</option>
+         </select>
+         <h3>Name & describe your event</h3>
+         <div class="field">
+            <label>Title</label>
+            <input v-model="event.title" type="text" placeholder="Add an event title"/>
+         </div>
+         <div class="field">
+            <label>Description</label>
+            <input v-model="event.description" type="text" placeholder="Add a description"/>
+         </div>
+         <h3>Where is your event?</h3>
+         <div class="field">
+            <label>Location</label>
+            <input v-model="event.location" type="text" placeholder="Add a location"/>
+         </div>
+         <h3>When is your event?</h3>
+         <div class="field">
+            <label>Date</label>
+            <datepicker v-model="event.date" placeholder="Select a date"/>
+         </div>
+         <div class="field">
+            <label>Select a time</label>
+            <select v-model="event.time">
+               <option v-for="time in times" :key="time">{{ time }}</option>
+            </select>
+         </div>
+         <input type="submit" class="button -fill-gradient" value="Submit"/>
+      </form>
+   </div>
 </template>
 
-<style scoped></style>
+<script>
+   // import { mapState, mapGetters } from 'vuex';
+   import Datepicker from 'vuejs-datepicker';
+
+  export default {
+    components: {
+      Datepicker
+    },
+    data() {
+      const times = [];
+      for (let i = 1; i <= 24; i++) {
+        times.push(i + ' :00')
+      }
+      return {
+        event: this.createFreshEventObject(),
+        categories: this.$store.state.categories,
+        times
+      }
+    },
+    methods: {
+      createEvent() {
+        this.$store.dispatch('createEvent', this.event)
+         .then(() => {
+           this.$router.push({
+             name: 'event-show',
+             params: { id: this.event.id }
+           });
+            this.event = this.createFreshEventObject()
+         })
+        .catch((error) => {
+          console.log('Catch error');
+          console.log(error)
+        })
+      },
+      createFreshEventObject() {
+        const user = this.$store.state.user;
+        const id = Math.floor(Math.random() * 10000000);
+        return {
+          id: id,
+          category: '',
+          organizer: user,
+          title: '',
+          description: '',
+          location: '',
+          date: '',
+          time: '',
+          attendees: []
+        }
+      }
+    }
+    // Using MapState one way
+    // computed: mapState({
+    //   userName: state => state.user.name,
+    //   userId: state => state.user.id,
+    //   categories: state => state.categories,
+    // })
+
+    // Using MapState with dot notation
+    // computed: mapState({
+    //   user: 'user',
+    //   categories: 'categories'
+    // })
+
+    // Using MapState but put all keys in an array
+    // computed: mapState(['user', 'categories'])
+
+    // Using MapState with object spread operator
+    // computed: {
+      // getEvent() {
+      //   return this.$store.getters.getEventById
+      // },
+    //   catLength() {
+    //     return this.$store.getters.catLength
+    //   },
+    //   ...mapGetters(['getEventById']),
+    //   ...mapState(['user', 'categories'])
+    // }
+  }
+</script>
+
+<style scoped>
+   .field {
+      margin-bottom: 24px;
+   }
+</style>
